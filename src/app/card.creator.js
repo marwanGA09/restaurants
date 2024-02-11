0a1a0d3e83abc7060eb98e508dea178fb9b43883
Adem Kedir <ademkedir724@gmail.com>
2024-02-11 14:56:15 +0300

    create testimony section dynamically

--- src/app/card.creator.js
+++ src/app/card.creator.js
@@ -1,10 +1,22 @@
 import { contentCreator } from "./content.creator";
 export function cardCreator(imgSource, heading, main) {
   const card = document.createElement("div");
+  card.classList.add("card");
   const img = document.createElement("img");
   img.src = imgSource;
   card.appendChild(img);
   card.appendChild(contentCreator(heading, "h2", "card__heading"));
   card.appendChild(contentCreator(main, "p", "card__content"));
+  return card;
+}
+
+export function cardCreatorTestimony(imgSource, quote, name) {
+  const card = document.createElement("div");
+  card.classList.add("quote-card");
+  const img = document.createElement("img");
+  img.src = imgSource;
+  card.appendChild(img);
+  card.appendChild(contentCreator(quote, "blockquote", "quote__content"));
+  card.appendChild(contentCreator(name, "h3", "quote__name"));
   return card;
 }
