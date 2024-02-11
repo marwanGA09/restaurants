8bf1f640ab273ffb4ec1a854dd1238d71a2f5b23
Adem Kedir <ademkedir724@gmail.com>
2024-02-11 14:28:16 +0300

    update style and index.js to integrate with menu container

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
 import "./style.scss";
 import "./template.html";
+
+import { log } from "./app/createheader";
+import { buttons } from "./app/createheader";
+import { footerElement } from "./app/createFooter";
+const container = document.querySelector(".container");
+const nav = document.querySelector(".nav");
+// create navigation
+nav.appendChild(log);
+buttons.forEach((bt) => {
+  nav.appendChild(bt);
+});
+// container.appendChild(nav);
+
+import { homeContainer, featureContainer } from "./app/homeCreator";
+// console.log(homeContainer);
+container.appendChild(homeContainer);
+container.appendChild(featureContainer);
+
+container.appendChild(footerElement);
