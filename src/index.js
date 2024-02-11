2291bcb1252ec88b063d4982c71f1130edea3aff
Adem Kedir <ademkedir724@gmail.com>
2024-02-11 22:12:21 +0300

    add eventlistener for bolton to change between pages

--- src/index.js
+++ src/index.js
@@ -4,29 +4,40 @@ import "./template.html";
 import { log } from "./app/createheader";
 import { buttons } from "./app/createheader";
 import { footerElement } from "./app/createFooter";
-const container = document.querySelector(".container");
+const container = document.querySelector(".sub-container");
 const nav = document.querySelector(".nav");
 nav.appendChild(log);
 buttons.forEach((bt) => {
   nav.appendChild(bt);
 });
 
-// *********** dynamically change
 import { homeContainer, featureContainer } from "./app/homeUtils/homeCreator";
 import { testimony } from "./app/homeUtils/testimonyCreator";
 import { createMenuCard } from "./app/menuUtilis/createMenu";
-container.appendChild(homeContainer);
-container.appendChild(featureContainer);
-
-container.appendChild(testimony);
-// test
 
 import img from "./imgs/shiro.jpg";
 import { chief, location } from "./app/contactUtils/createUtils";
-container.appendChild(createMenuCard(img));
 
-container.appendChild(chief);
-container.appendChild(location);
-
-// *********** always on the page
+container.appendChild(homeContainer);
+container.appendChild(featureContainer);
+container.appendChild(testimony);
 container.appendChild(footerElement);
+buttons.forEach((bt) => {
+  bt.addEventListener("click", (ev) => {
+    const target = ev.target.id;
+    container.replaceChildren();
+    if (target === "home") {
+      container.appendChild(homeContainer);
+      container.appendChild(featureContainer);
+      container.appendChild(testimony);
+      container.appendChild(footerElement);
+    } else if (target === "menu") {
+      container.appendChild(createMenuCard(img));
+      container.appendChild(footerElement);
+    } else if (target === "contact") {
+      container.appendChild(chief);
+      container.appendChild(location);
+      container.appendChild(footerElement);
+    }
+  });
+});
