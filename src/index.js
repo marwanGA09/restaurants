2e816eeb157fe9a075c3717424ce3d4090f746e2
Adem Kedir <ademkedir724@gmail.com>
2024-02-11 17:08:23 +0300

    connect contactmodule with index.js

--- src/index.js
+++ src/index.js
@@ -6,16 +6,27 @@ import { buttons } from "./app/createheader";
 import { footerElement } from "./app/createFooter";
 const container = document.querySelector(".container");
 const nav = document.querySelector(".nav");
-// create navigation
 nav.appendChild(log);
 buttons.forEach((bt) => {
   nav.appendChild(bt);
 });
-// container.appendChild(nav);
 
-import { homeContainer, featureContainer } from "./app/homeCreator";
-// console.log(homeContainer);
+// *********** dynamically change
+import { homeContainer, featureContainer } from "./app/homeUtils/homeCreator";
+import { testimony } from "./app/homeUtils/testimonyCreator";
+import { createMenuCard } from "./app/menuUtilis/createMenu";
 container.appendChild(homeContainer);
 container.appendChild(featureContainer);
 
+container.appendChild(testimony);
+// test
+
+import img from "./imgs/shiro.jpg";
+import { chief, location } from "./app/contactUtils/createUtils";
+container.appendChild(createMenuCard(img));
+
+container.appendChild(chief);
+container.appendChild(location);
+
+// *********** always on the page
 container.appendChild(footerElement);
